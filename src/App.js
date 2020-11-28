05e9bf7a6423d226b638feb1f1a40a75b7546d94
Mandi <haase1020@gmail.com>
2020-11-28 18:41:20 -0500

    finish up tutorial from Ben Awad Reactjs series

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
-import React,{ Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
-class Header extends Component{
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
   render() {
     return (
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-
-    )
+      <div className="App">
+        <TodoList />
+      </div>
+    );
   }
 }
-
-  class App extends Component {
-    render() {
-      return (
-        <div className="App">
-         <p>to get started, start typing</p>
-        </div>
-      );
-
-    }
-  }
-  
-
 
 export default App;
